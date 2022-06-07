31fc314e1cfb103235c7308b5870ffd79f4b2fd9
sana-miraki <sanamiraki95@gmail.com>
2022-06-07 16:23:09 +0300

    first commit

---
+++ test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Sana Miraki on 6/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+    
+    
+    
+    
+
+}
+
